b5a9f375fc7d1b0b51e34d3f11acd09f6da8875a
cryptitalk <cryptitalk@cryptitalk.com>
2023-11-12 19:47:00 -0800

    determin prd and test env

--- src/main.js
+++ src/main.js
@@ -8,13 +8,21 @@ import axios from 'axios'
 import Mint from 'mint-ui'
 import 'mint-ui/lib/style.css'
 import VueTouch from 'vue-touch'
-Vue.use(VueTouch,{name: 'v-touch'})
+Vue.use(VueTouch, { name: 'v-touch' })
 
 //axios.defaults.baseURL = 'http://host.docker.internal:5000/'   //加前缀
 //axios.defaults.baseURL = 'http://localhost:5000/'   //加前缀
-axios.defaults.baseURL = 'https://byzql.nas.cpolar.cn/'   //加前缀
-//axios.defaults.withCredentials = true   //允许跨域
-global.axios = axios    //全局化
+
+if (process.env.NODE_ENV === 'production') {
+  axios.defaults.baseURL = 'https://main-wjaxre4ena-uc.a.run.app/'   //加前缀
+  global.axios = axios    //全局化
+  console.log("in production")
+} else {
+  axios.defaults.baseURL = 'https://byzql.nas.cpolar.cn/'   //加前缀
+  global.axios = axios    //全局化
+  console.log("in test")
+}
+
 global.isInitiated = false
 global.lastScrollY = 0
 
@@ -27,14 +35,14 @@ Vue.filter('truncate', function (value, length) {
 
 Vue.prototype.$queue = [];
 
-Vue.prototype.$updateQueue = function(item) {
+Vue.prototype.$updateQueue = function (item) {
   this.$queue.push(item);
   if (this.$queue.length > 4) {
     this.$queue.shift(); // Remove the first (oldest) item
   }
 };
 
-Vue.prototype.$queueToString = function() {
+Vue.prototype.$queueToString = function () {
   if (this.$queue.length === 0) {
     return '0';
   }
